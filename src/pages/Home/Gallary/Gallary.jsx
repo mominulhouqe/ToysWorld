166acdbb1b8738cf52d1f1210926bf6470c633ae
mominul <ulhouqemomin39@gmail.com>
2023-05-18 15:55:07 +0600

    updated gallary

--- src/pages/Home/Gallary/Gallary.jsx
+++ src/pages/Home/Gallary/Gallary.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-const Gallary = () => {
-    return (
-        <div>
+const Gallery = () => {
+  const [gallery, setGallery] = useState([]);
 
-            <h1 className='text-5xl my-10 text-center font-bold'>Popular Products Gallary</h1>
-            <hr className='my-16'/>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 font-serif justify-center">
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
-                    <img src="https://i.ibb.co/dGmQLgn/my-img30.jpg" alt="Toy 1" className="rounded-lg shadow-lg" />
-                    <p className="mt-2 text-lg font-bold">The Whimsical Woodland Friends</p>
-                    <p className="mt-1">Step into an enchanted forest where adorable woodland creatures come to life. This delightful collection of plush toys features cuddly foxes, wise owls, and playful squirrels, each one ready to be your loyal companion on magical adventures.</p>
-                </div>
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
-                    <img src="https://i.ibb.co/WptGJ0K/my-img29.jpg" alt="Toy 2" className="rounded-lg shadow-lg" />
-                    <p className="mt-2 text-lg font-bold">The Dazzling Princess Castle</p>
-                    <p className="mt-1">Enter a world of fairytales with this exquisite princess castle toy. Its shimmering towers and delicate details will transport you to a realm of royalty and elegance. Unleash your inner prince or princess and create enchanting stories in this magical castle.</p>
-                </div>
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
-                    <img src="https://i.ibb.co/s3YLGsL/my-img14.jpg" alt="Toy 3" className="rounded-lg shadow-lg" />
-                    <p className="mt-2 text-lg font-bold">The Adventure Seeker's Kit</p>
-                    <p className="mt-1">Calling all adventurers! This kit is designed for the brave souls who seek thrilling quests and daring escapades. With a compass, a treasure map, and a trusty explorer's hat, you'll be ready to conquer uncharted territories and unlock hidden treasures.</p>
-                </div>
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
-                    <img src="https://i.ibb.co/WvGsc0G/my-img27.jpg" alt="Toy 4" className="rounded-lg shadow-lg" />
-                    <p className="mt-2 text-lg font-bold">The Magical Music Box</p>
-                    <p className="mt-1">Turn the key and let the enchanting melodies transport you to a world of magic and whimsy. This intricately designed music box features delicate ballerinas twirling gracefully to the soothing tunes, creating a mesmerizing spectacle for the eyes and ears.</p>
-                </div>
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
-                    <img src="https://i.ibb.co/ncF91MR/my-img26.jpg" alt="Toy 5" className="rounded-lg shadow-lg" />
-                    <p className="mt-2 text-lg font-bold">The Racing Champions Set</p>
-                    <p className="mt-1">Rev up your engines and get ready for an adrenaline-fueled race! This dynamic racing set includes sleek cars, a winding track, and a cheering crowd. Experience the thrill of victory as you compete against friends or embark on solo racing adventures.</p>
-                </div>
+  useEffect(() => {
+    AOS.init();
+    fetch('gallary.json')
+      .then(res => res.json())
+      .then(data => setGallery(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <div className='w-1/2 mx-auto'>
+        <h1 className='text-5xl my-10 text-center font-bold'>Popular Products Gallery</h1>
+        <p className=''>Nisl vel pretium lectus quam id leo in vitae. Eros in cursus turpis massa. Blandit massa enim nec dui nunc. Turpis in eu mi bibendum neque egestas. Viverra nam libero justo laoreet sit amet cursus sit.</p>
+      </div>
+      <hr className='my-16' />
+      <div className="font-serif flex flex-wrap justify-center space-x-5 space-y-5">
+        {gallery.map(item => (
+          <div key={item.id} className="card bg-base-100 shadow-xl w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5" data-aos="fade-right" data-aos-duration="800">
+            <figure className="w-full">
+              <img src={item.img} alt={`Toy ${item.id}`} className="rounded-lg h-auto shadow-lg" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{item.title}</h2>
+              <p>{item.description}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Gallary;
+export default Gallery;
--- src/pages/Home/ToysCatagory/ToysCatagory.jsx
+++ src/pages/Home/ToysCatagory/ToysCatagory.jsx
@@ -1,61 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const ToysCategory = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        fetch('shops.json')
-            .then((res) => res.json())
-            .then(data => setItems(data.categories)
-            )
-    }, []);
+  useEffect(() => {
+    AOS.init();
+    fetch('shops.json')
+      .then(res => res.json())
+      .then(data => setItems(data.categories));
+  }, []);
 
-    return (
-        <div className="p-4 font-serif mb-4  mx-auto bg-slate-100">
-            <h2 className="text-5xl my-14 font-extrabold mb-4 text-center">Shop by Category</h2>
-            <hr  className='text-warning'/>
-            <Tabs className="text-center">
-                <TabList className="flex mb-14 align-middle justify-center text-1xl my-16 ">
-                    {items.map((category, index) => (
-                        <Tab
-                            key={index}
-                            className="px-4 py-2 bg-gray-200 text-gray-800 rounded cursor-pointer mr-2"
-                        >
-                            {category.name}
-                        </Tab>
-                    ))}
-                </TabList>
+  return (
+    <div className="p-4 font-serif mb-4  mx-auto bg-slate-100">
+      <h2 className="text-5xl my-14 font-extrabold mb-4 text-center">Shop by Category</h2>
+      <hr className='text-warning' />
+      <Tabs className="text-center">
+        <TabList className="flex mb-14 align-middle justify-center text-1xl my-16 ">
+          {items.map((category, index) => (
+            <Tab
+              key={index}
+              className="px-4 py-2 bg-gray-200 text-gray-800 rounded cursor-pointer mr-2"
+            >
+              {category.name}
+            </Tab>
+          ))}
+        </TabList>
 
-                {items.map((category, index) => (
-                    <TabPanel key={index}>
-                        {category.subCategories.map((subCategory, subIndex) => (
-                            <div key={subIndex}>
-                                <h3 className="text-lg font-semibold mt-4 mb-2">{subCategory.name}</h3>
-                                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4">
-                                    {subCategory.toys.map((toy, toyIndex) => (
-                                        <div key={toyIndex} className="bg-white p-4 rounded shadow">
-                                            <img src={toy.picture} alt={toy.name} className="w-full h-40 object-cover mb-2" />
-                                            <h4 className="text-base font-semibold">{toy.name}</h4>
-                                            <p className="text-gray-600">Price: ${toy.price}</p>
-                                            <p className="text-gray-600">Rating: {toy.rating}</p>
-                                            <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
-                                                View Details
-                                            </button>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-                    </TabPanel>
-                ))}
-            </Tabs>
-
-          
-
-        </div>
-    );
+        {items.map((category, index) => (
+          <TabPanel key={index}>
+            {category.subCategories.map((subCategory, subIndex) => (
+              <div key={subIndex}>
+                <h3 className="text-lg font-semibold mt-4 mb-2">{subCategory.name}</h3>
+                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4">
+                  {subCategory.toys.map((toy, toyIndex) => (
+                    <div
+                      key={toyIndex}
+                      data-aos="fade-left"
+                      data-aos-delay={toyIndex * 100} // Add delay for each card animation
+                      className="bg-white p-4 rounded-2xl shadow"
+                    >
+                      <img src={toy.picture} alt={toy.name} className="w-full h-40 object-cover mb-2" />
+                      <h4 className="text-base font-semibold">{toy.name}</h4>
+                      <p className="text-gray-600">Price: ${toy.price}</p>
+                      <p className="text-gray-600">Rating: {toy.rating}</p>
+                      <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+                        View Details
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </TabPanel>
+        ))}
+      </Tabs>
+    </div>
+  );
 };
 
 export default ToysCategory;
