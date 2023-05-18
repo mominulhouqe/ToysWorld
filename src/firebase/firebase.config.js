9ae3ef77ddd4dadc2b10e543f6a16666b15a139f
mominul <ulhouqemomin39@gmail.com>
2023-05-18 07:28:58 +0600

    added authprovider

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6JijxSw3yHaozJzu2LL2ZCMO7w6xek4M",
+  authDomain: "toys-world-aa3b0.firebaseapp.com",
+  projectId: "toys-world-aa3b0",
+  storageBucket: "toys-world-aa3b0.appspot.com",
+  messagingSenderId: "809311638480",
+  appId: "1:809311638480:web:1bce2b67d86d6ac2189fd5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -6,17 +6,17 @@ const Footer = () => {
 
             <footer className="bg-gray-900 text-white py-10">
                 <div className="container mx-auto flex flex-wrap justify-between">
-                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4">
+                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4 ">
                         <img src="https://images-platform.99static.com//qJ3u8FABhDvDacTrJfkMzHvVFCM=/378x266:1622x1510/fit-in/500x500/99designs-contests-attachments/138/138432/attachment_138432570" alt="Logo" className="h-8" />
                             <p className="mt-4 text-gray-400">ToysRUs - Bringing Joy to Kids Everywhere</p>
                             <p className="text-gray-400 text-sm mt-2">&copy; 2023 ToysRUs. All rights reserved.</p>
                     </div>
-                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4">
+                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4 ">
                         <h5 className="font-semibold text-gray-400 mb-2">Contact Us</h5>
                         <p className="text-gray-400">Email: info@toysrus.com</p>
                         <p className="text-gray-400">Phone: +1 123-456-7890</p>
                     </div>
-                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4">
+                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4 ">
                         <h5 className="font-semibold text-gray-400 mb-2">Follow Us</h5>
                         <div className="flex">
                             <a href="#" className="mr-2 text-gray-400 hover:text-blue-500 transition-colors duration-300"> <FaFacebook></FaFacebook> </a>
@@ -27,7 +27,7 @@ const Footer = () => {
                             <a href="#" className="mr-2 text-gray-400 hover:text-blue-500 transition-colors duration-300"><FaPinterest></FaPinterest> </a>
                         </div>
                     </div>
-                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4">
+                    <div className="w-full md:w-1/3 lg:w-1/4 mb-4 ">
                         <h5 className="font-semibold text-gray-400 mb-2">Address</h5>
                         <p className="text-gray-400">123 Toy Street</p>
                         <p className="text-gray-400">Sylhet Town, State ZIP</p>
