15c4f9eb13a91abd6f8ac57ecd519f2d0cb97e4b
mominul <ulhouqemomin39@gmail.com>
2023-05-20 18:34:19 +0600

    updated single data

--- src/pages/ToysDetails/SingleData/SingleData.jsx
+++ src/pages/ToysDetails/SingleData/SingleData.jsx
@@ -4,64 +4,54 @@ import useTitle from '../../../hooks/useTitle';
 import { FaBookmark, FaHeart, FaRegStar, FaStar } from "react-icons/fa";
 
 const SingleData = () => {
+  const [toys, setToys] = useState(null);
+  useTitle('ToysDetails');
 
-    const [toys, setToys] = useState(null);
-    useTitle('ToysDetails');
-
-    useEffect(() => {
-        const storedToyData = localStorage.getItem("SingleToys");
-        if (storedToyData) {
-            const toy = JSON.parse(storedToyData);
-            setToys(toy);
-        }
-    }, []);
-
-    if (!toys) {
-        return (
-            <div className="flex justify-center mt-20">
-                <progress className="progress w-56"></progress>
-            </div>
-        );
+  useEffect(() => {
+    const storedToyData = localStorage.getItem("SingleToys");
+    if (storedToyData) {
+      const toy = JSON.parse(storedToyData);
+      setToys(toy);
     }
+  }, []);
 
+  if (!toys) {
     return (
-        <div>
-            <div className="flex justify-center mt-10 mb-10">
-
-
-                <div className="card bg-base-100 shadow-xl w-96 relative overflow-hidden">
-                    <figure>
-                        <img className="w-full h-96" src={toys?.picture} alt={toys?.name} />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title font-bold">{toys?.name}</h2>
-                        <p>{toys?.description}</p>
-                        <p>Price: ${toys?.price}</p>
-                        <p>
-                            <span className="flex">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                    <path d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z" />
-                                </svg>
-                                {toys?.likes}
-                            </span>
-                        </p>
-                        <div className="rating">
-                            <Rating
-                                emptySymbol={<FaRegStar className="mask mask-star" />}
-                                fullSymbol={<FaStar className="mask mask-star" />}
-                                initialRating={toys?.rating}
-                                readonly
-                            />
-                        </div>
-                    </div>
-                    <div className="heart-animation absolute top-0 right-0 m-2">
-                        <FaBookmark className="h-10 w-10" />
-                    </div>
-                    <div className="card-overlay absolute inset-0 bg-gray-900 bg-opacity-0 hover:bg-opacity-20 transition-opacity duration-300"></div>
-                </div>
-            </div>
-        </div>
+      <div className="flex justify-center mt-20">
+        <progress className="progress w-56"></progress>
+      </div>
     );
+  }
+
+  return (
+    <div className="bg-base-200 min-h-screen">
+      <div className="container mx-auto px-4 py-12">
+        <div className="bg-white rounded-lg shadow-xl items-center justify-center gap-10 md:flex">
+          <div className="md:flex-shrink-0">
+            <img src={toys?.picture} alt={toys?.name} className="h-80 w-full object-cover rounded-lg md:w-80" />
+          </div>
+          <div className="p-6">
+            <h1 className="text-3xl font-bold">Toys Name {toys?.name}</h1>
+            <h1 className="text-2xl font-semibold">Seller Name: {toys?.sellerName}</h1>
+            <p className="text-gray-600 py-2">Seller Email: {toys?.sellerEmail}</p>
+            <p className="text-gray-600 py-2">Available Quantity: {toys?.quantity}</p>
+            <p className="text-gray-600 py-2">Price: ${toys?.price}</p>
+            <p className="py-2">Description : {toys?.description}</p>
+            <div className="flex items-center space-x-1 py-2">
+              <Rating
+                emptySymbol={<FaRegStar className="text-yellow-400 h-6 w-6" />}
+                fullSymbol={<FaStar className="text-yellow-400 h-6 w-6" />}
+                initialRating={toys?.rating}
+                readonly
+              />
+              <span className="text-gray-600">({toys?.rating})</span>
+            </div>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600">Order Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleData;
