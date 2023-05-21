01be09aee985ac5600e7031e340219e0a52c9abc
mominul <ulhouqemomin39@gmail.com>
2023-05-21 22:58:31 +0600

    updated sweet aleart

--- src/pages/AddToys/updated/Updated.jsx
+++ src/pages/AddToys/updated/Updated.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useLoaderData } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
+import useTitle from '../../../hooks/useTitle';
 
 const UpdatedForm = () => {
   const { register, handleSubmit, reset } = useForm();
   const loader = useLoaderData();
   const { description, quantity, price, _id, name } = loader;
 
+  useTitle('Update');
+  
   const onSubmit = async (data, e) => {
     try {
       const response = await fetch(`https://toys-server-mu.vercel.app/addToys/${_id}`, {
@@ -19,21 +22,28 @@ const UpdatedForm = () => {
       });
 
       if (response.ok) {
-        toast.success('Toy updated successfully!');
+        Swal.fire({
+          icon: 'success',
+          title: 'Toy updated successfully!',
+        });
+
         reset(); // Reset the form
       } else {
         throw new Error('Failed to update toy.');
       }
     } catch (error) {
       console.error(error);
-      toast.error('Failed to update toy.');
+      Swal.fire({
+        icon: 'error',
+        title: 'Failed to update toy.',
+      });
     }
   };
 
   return (
     <div className="my-14 lg:w-1/2 mx-auto">
       <h4 className="text-3xl text-center font-bold mb-4">Update Your Product</h4>
-      <h4 className="text-2xl text-center font-bold my-4">Product Name : {name} </h4>
+      <h4 className="text-2xl text-center font-bold my-4">Product Name: {name}</h4>
       <div>
         <form className="bg-white rounded border shadow-lg p-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
